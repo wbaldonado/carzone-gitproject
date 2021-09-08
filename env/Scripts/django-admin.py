a5b1718379556ecfc5673e746e3a36cf43164b0d
Winston Baldonado <wbaldonado2002@yahoo.com>
2021-09-08 17:00:41 -0500

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\winston\documents\projects\carzone-project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
